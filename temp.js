eb97ac45defda19cc48096e859bf42a2e27cad30
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-30 20:00:23 +0300

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,11 @@
+//#region menu toggler
+let navLinks = document.getElementById("navLinks")
+
+function showMenu() {
+    navLinks.style.right = '0';
+}
+
+function hideMenu() {
+    navLinks.style.right = '-200px';
+}
+//#endregion menu toggler
